f63eccfc09e7037d33387200c4a037d45da2d214
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-09 19:03:26 +0100

    Add const head and css

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 import initialProperties from 'json!./src/initialProperties.json';
+
+export default window.define([], function() {
+  const css = localCSS.toString();
+
+  const head = document.querySelector('head');
+});
