562107b2e60eb5e1d0c2e9173ddabbabd5612c2d
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-10 19:59:29 +0100

    UPDATE paint.js

--- src/scripts/paint.js
+++ src/scripts/paint.js
@@ -1,7 +1,13 @@
+import * as d3 from 'd3';
+
 export default function($element, layout) {
   var element = $element[0];
   //   console.log(element);
 
   var container = element.querySelector('.chart-cont');
+  var rect = container.getBoundingClientRect();
+  var height = rect.height;
+  var width = rect.width;
+
   var svg = d3.select(element.querySelector('svg'));
 }
