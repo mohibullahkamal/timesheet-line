ce5f6b24d7a3db817c33a07428e6c00a179c981a
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-10 19:39:39 +0100

    Add paint.js

---
+++ src/scripts/paint.js
@@ -0,0 +1,7 @@
+export default function($element, layout) {
+  var element = $element[0];
+  //   console.log(element);
+
+  var container = element.querySelector('.chart-cont');
+  var svg = d3.select(element.querySelector('svg'));
+}
