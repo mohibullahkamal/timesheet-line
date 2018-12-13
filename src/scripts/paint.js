e37e3bdbb40f53964f21ef4376cc5d4a21fe780f
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-13 21:14:48 +0100

    Update paint.js

--- src/scripts/paint.js
+++ src/scripts/paint.js
@@ -1,8 +1,20 @@
 import * as d3 from 'd3';
 
 export default function($element, layout) {
-  var element = $element[0];
-  //   console.log(element);
+  var qMarix = layout.qHyperCube.qDataPage[0].qMarix.sort((a, b) => {
+    if (a[0].qNum < b[0].qNum) return -1;
+    else return 1;
+  });
+
+  var margin = {
+    top: 10,
+    left: 10,
+    right: 50,
+    bottom: 10
+  };
+
+  // var element = $element[0];
+  // console.log(element);
 
   var container = element.querySelector('.chart-cont');
   var rect = container.getBoundingClientRect();
@@ -15,12 +27,7 @@ export default function($element, layout) {
     .attr('width', width)
     .attr('height', height);
 
-  var margin = {
-    top: 10,
-    left: 10,
-    right: 50,
-    bottom: 10
-  };
+  var container = element;
 
   var plot = svg
     .append('g')
