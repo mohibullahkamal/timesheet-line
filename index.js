67e9479aaf4ca0d3e3eace48d637c676f6c763b2
mogit2020 <mohibullah.kamal@gmail.com>
2018-12-21 18:02:16 +0100

    imports added

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import initialProperties from 'json!./src/initialProperties.json';
+import initialProperties from "json!./src/initialProperties.json";
+import definition from "./src/definition.js";
+import controller from "./src/controller.js";
+import paint from "./src/paint.js";
+import resin from "./src/resize.js";
+import template from "./src/template.html";
+import support from "json!./src/support.json";
+import tocatCSS from "!css?minimize!sass!wrap?ext!./src/style.scss";
 
-export default window.define([], function() {
+export default window.define([], function () {
   const css = localCSS.toString();
 
   const head = document.querySelector('head');
